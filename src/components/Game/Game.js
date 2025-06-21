79ef94762d7984932dc1f2a4cf61f7aff3335c8b
Yuanzhen <yuanzhendai@gmail.com>
2025-06-21 21:07:45 +0200

    Exercise 4: Game Logic

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -15,7 +15,7 @@ function Game() {
 
   return (
     <>
-      <Guess guesses={guesses} />
+      <Guess guesses={guesses} answer={answer} />
       <GuessInput guesses={guesses} setGuesses={setGuesses} />
     </>
   );
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,18 +1,37 @@
 import React from "react";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ guesses }) {
+function Guess({ guesses, answer }) {
   return (
     <div className="guess-results">
-      {Array.from({ length: NUM_OF_GUESSES_ALLOWED }, (_, index) => (
-        <p className="guess" key={index}>
-          {Array.from({ length: 5 }, (_, subIndex) => (
-            <span className="cell" key={subIndex}>
-              {guesses[index] && guesses[index][subIndex]}
-            </span>
-          ))}
-        </p>
-      ))}
+      {Array.from({ length: NUM_OF_GUESSES_ALLOWED }, (_, index) => {
+        if (!guesses[index]) {
+          return (
+            <p className="guess" key={index}>
+              {Array.from({ length: 5 }, (_, subIndex) => (
+                <span className="cell" key={subIndex}>
+                  {" "}
+                </span>
+              ))}
+            </p>
+          );
+        }
+
+        const guessStatus = checkGuess(guesses[index], answer);
+        return (
+          <p className="guess" key={index}>
+            {Array.from({ length: 5 }, (_, subIndex) => (
+              <span
+                className={`cell ${guessStatus[subIndex]["status"]}`}
+                key={subIndex}
+              >
+                {guessStatus[subIndex]["letter"]}
+              </span>
+            ))}
+          </p>
+        );
+      })}
     </div>
   );
 }
