35519091c7975018a925d657913945fc1a539eb0
Yuanzhen <yuanzhendai@gmail.com>
2025-06-21 20:54:23 +0200

    Exercise 3: Guess Slots

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 function Guess({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess, index) => (
+      {Array.from({ length: NUM_OF_GUESSES_ALLOWED }, (_, index) => (
         <p className="guess" key={index}>
-          {guess}
+          {Array.from({ length: 5 }, (_, subIndex) => (
+            <span className="cell" key={subIndex}>
+              {guesses[index] && guesses[index][subIndex]}
+            </span>
+          ))}
         </p>
       ))}
     </div>
